4804ea2b7176be2d6437a70b07027ee91e3b9d8b
natumeno <44020273+natumeno@users.noreply.github.com>
2022-04-25 10:16:50 +0900

    Add files via upload

---
+++ abc237-b/abc237-b/abc237-b.cpp
@@ -0,0 +1,49 @@
+﻿// abc237-b.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <iomanip>
+#include <math.h>
+#define rep(i,n) for(ll i=0;i<n;++i)
+typedef long long ll;
+
+int main() {
+	ll h, w;
+	std::cin >> h >> w;
+	std::vector<std::vector<ll>> a(h, std::vector<ll>(w));
+	std::vector<std::vector<ll>> b(w, std::vector<ll>(h));
+	rep(i, h) {
+		rep(j, w) {
+			std::cin >> a[i][j];
+		}
+	}
+
+	rep(i, w) {
+		rep(j, h) {
+			if (j == 0) {
+				std::cout << a[j][i];
+			}
+			else {
+				std::cout << " " << a[j][i];
+			}
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
